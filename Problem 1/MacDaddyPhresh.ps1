10575e431fc68e29f4e1448bef37e2eda3dce7ed
ayoung0987 <ayoung0987@gmail.com>
2014-08-28 15:50:51 -0500

    P1- Create Function continued
    Sigh... Rough day dude...   tried working on this all day today.  my
    head hurts.  then boots comes up and makes it sound easy... and.. ya...
    Did you put my on page 200 of the learning book?  lol

--- Problem 1/MacDaddyPhresh.ps1
+++ Problem 1/MacDaddyPhresh.ps1
@@ -7,3 +7,7 @@ New-Item -Path "$location" -Name "$name" -Type File
 # Create file using above function and add the path and file name.
 Create-File "C:\Users\ayoung2\Documents\GitHub\powershell\Problem 1\Totally Confused.txt"
 
+
+#Output Current date and time, Current ip address of the system, Current working directory, Current user running this script to the Totally Confused.txt each on a new line.
+Function Time {Get-Date}
+$IP = Get-wmiobject win32_networkadapterconfiguration | select IPaddress
